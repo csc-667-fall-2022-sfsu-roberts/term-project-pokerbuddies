36fa3d7ebf69304acfa96f3e4a61d154752e1437
celiarr <cramireznavarrete@mail.sfsu.edu>
2022-10-10 18:17:23 -0700

    pushing project

--- 667-pokerbuddies/app.js
+++ 667-pokerbuddies/app.js
@@ -4,14 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-
 if(process.env.NODE_ENV === 'development') {
-  console.log("HEELLELEE");
   require("dotenv").config();
-  console.log("Ran the dot");
 }
-
 var testRouter = require('./routes/tests');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -31,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tests', testRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- 667-pokerbuddies/db/index.js
+++ 667-pokerbuddies/db/index.js
@@ -1,7 +1,21 @@
+
+/*const pgp = require('pg-promise')();
+const db = pgp('postgres://students:student@localhost:5432/playerstats')
+
+db.one('SELECT $1 AS value', 123)
+  .then((data) => {
+    console.log('DATA:', data.value)
+  })
+  .catch((error) => {
+    console.log('ERROR:', error)
+  })
+//const connection = pgp(db);//DATABASE_URL);
+const connection = pgp(process.env.db);
+module.exports = connection;*/
 console.log({
-    DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV
-  });
+  DATABASE_URL: process.env.DATABASE_URL,
+  NODE_ENV: process.env.NODE_ENV
+});
 const pgp = require('pg-promise')();
-const connection = pgp('postgres://postgres:student@localhost:5432/PlayerStats');
+const connection = pgp('postgres://students:student@localhost:5432/playerstats');
 module.exports = connection;
