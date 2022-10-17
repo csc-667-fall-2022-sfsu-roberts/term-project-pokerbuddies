a559cccef30a36e26465cef7eb5fd035e80c2fa9
deadlepricon <rm92691@gmail.com>
2022-10-17 15:38:54 -0700

    homepage

--- 667-pokerbuddies/routes/index.js
+++ 667-pokerbuddies/routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
+let reqPath = path.join(__dirname, '../');
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(reqPath+'/FrontEnd/HTML/Home.html');
 });
 
 module.exports = router;
