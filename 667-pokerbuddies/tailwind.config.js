e871277bce8077b38cd136caba44750a2ed86dc7
jonathanluu0 <jonathanluu0@gmail.com>
2022-10-30 15:29:08 -0700

    tailwind

--- 667-pokerbuddies/tailwind.config.js
+++ 667-pokerbuddies/tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   content: [
     "./**/*.{html, js}"],
+=======
+  content: ["./src/**/*.{html,js}"],
+>>>>>>> a506264f2c4f56aa7504784aea4ef74844821c31
   theme: {
     extend: {},
   },
