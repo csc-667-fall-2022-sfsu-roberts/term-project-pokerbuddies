15c5d67767e2b8b872493cc0a943f263a797c21e
celiarr <cramireznavarrete@mail.sfsu.edu>
2022-11-04 12:14:53 -0700

    fixed some routes and installed node-modules again

---
+++ 667-pokerbuddies/routes/registration.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+let reqPath = path.join(__dirname, '../');
+router.get('/', function(req, res, next) {
+  res.sendFile(reqPath+'/FrontEnd/HTML/Registration.html');
+});
+
+module.exports = router;
