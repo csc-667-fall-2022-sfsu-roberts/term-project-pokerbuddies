b490ae6ce190f9edb4e33ea177338371c804b98a
celiarr <cramireznavarrete@mail.sfsu.edu>
2022-11-14 16:05:16 -0800

    added new routes

--- 667-pokerbuddies/routes/games.js
+++ 667-pokerbuddies/routes/games.js
@@ -7,9 +7,9 @@ let reqPath = path.join(__dirname, '../');
 router.get('/', function(req, res, next) {
   res.sendFile(reqPath+'/FrontEnd/HTML/Games.html');
 });
-//routes to home page
-router.get('/home', function(req, res) {
-  res.sendFile(path.join(reqPath, '/FrontEnd/HTML/Home.html'));
+//routes to joinSession page
+router.get('/joinSession', function(req, res) {
+  res.sendFile(path.join(reqPath, '/FrontEnd/HTML/JoinSession.html'));
 });
 
 module.exports = router;
