e72604c1cd59a99ae9d77ea2119ec60483d21636
deadlepricon <rm92691@gmail.com>
2022-11-23 18:01:23 -0800

    card, player, deck and some game

--- 667-pokerbuddies/Server/Player.js
+++ 667-pokerbuddies/Server/Player.js
@@ -1,15 +1,44 @@
-class Player {
-    constructor(){
-        this.name = "temp name";
-        this.id = 0;
-        this.chipAmount = 0;
+const Player = function (name, socket){
+    this.userName = name;
+    this.cards = [];
+    this.socket = socket;
+    this.currentCard = null;
+    this.chips = 100;
+    this.buyIn = 0;
+    this.status = '';
+    this.blind = '';
+    this.allIn = false;
+    this.against = false;
+
+
+    this.addCard = (card) =>{
+        this.cards.push(card);
+    };
+
+    this.setStatus = (data) =>{
+        this.status = data;
     }
 
-    getChips(chips){
-        this.chipAmount = chips;
+    this.setBlind = (data) =>{
+        this.blind = data;
     }
 
-    getId(id) {
-        this.id = id;
+    this.getName = () =>{
+        return this.name;
     }
-}
+
+    this.getStatus = () =>{
+        return this.status;
+    };
+
+    this.getBlind = () =>{
+        return this.blind;
+    }
+
+    this.emit = (event, pay) => {
+        this.socket.emit(event,pay);
+    }
+
+};
+
+module.exports = Player;
--- 667-pokerbuddies/app.js
+++ 667-pokerbuddies/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const socket = require('socket.io');
+const http = require('http');
 
 if(process.env.NODE_ENV === 'development') {
   require("dotenv").config();
@@ -28,6 +30,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+const server = http.createServer(app);
+
+const io = socket(server);
+
+server.on('error',(err) => {
+  console.log(err);
+});
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
