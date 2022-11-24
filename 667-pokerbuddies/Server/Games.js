7a9ec55e38fec4b8d8382986cec0b70d0c2d045c
deadlepricon <rm92691@gmail.com>
2022-11-24 15:23:58 -0800

    some more to game

--- 667-pokerbuddies/Server/Games.js
+++ 667-pokerbuddies/Server/Games.js
@@ -25,6 +25,118 @@ const Games = function (id, name){
 
     const constructor = (function () {})(this);
 
-    
+    this.startGame = () =>{
+
+    };
+
+    this.dealCards = () =>{
+        this.deck.shuffle();
+        for(let i =0; i < this.players.length; i++){
+            this.players.cards =[];
+            for(let j = 0; j<2; j++){
+                this.players.addCard(this.deck.deal());
+            }
+        }
+
+    };
+
+    this.fold = (socket) =>{
+
+    };
+
+    this.call = (socket) =>{
+
+    };
+
+    this.bet = (socket, bet) =>{
+
+    };
+
+    this.check = (socket) =>{
+
+    };
+
+    this.raise = (socket, bet) =>{
+
+    };
+
+    this.allowedMoves = (socket) =>{
+
+    };
+
+    this.addNewPlayer = (playerName, socket) =>{
+        const player = new Player(playerName, socket);
+        this.players.push(player);
+        return player;
+    };
+
+    this.storeData = () =>{
+
+    };
+
+    this.assignBlinds = () =>{
+
+    };
+
+    this.newRound = () =>{
+
+    };
+
+    this.stages = () =>{
+
+    };
+
+    this.display = () =>{
+
+    };
+
+    this.getPot = () =>{
+
+    };
+
+    this.getPlayerBet = () =>{
+
+    };
+
+    this.getMaxBet = () =>{
+
+    };
+
+    this.isPlayerChecked = () =>{
+
+    };
+
+    this.getFirstTurn = () => {
+
+    };
+
+    this.updateStages = () =>{
+
+    };
+
+    this.nextPlayerTurn = () =>{
+
+    };
+
+    this.getWinnings = () =>{
+
+    };
+
+    this.checkHand = () =>{
+
+    };
+
+    this.revealHands = () =>{
+
+    };
+
+    this.setPlayerCards = (one_card) =>{
+
+    };
+
+
+
 
 }
+
+module.exports = Games;
