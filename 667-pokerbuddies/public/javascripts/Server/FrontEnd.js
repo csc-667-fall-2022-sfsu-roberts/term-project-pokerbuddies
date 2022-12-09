938211d4f416446c26beccbc71309c3d60a1cedd
celiarr <cramireznavarrete@mail.sfsu.edu>
2022-12-08 16:41:01 -0800

    fixed accounts page

---
+++ 667-pokerbuddies/public/javascripts/Server/FrontEnd.js
@@ -0,0 +1,9 @@
+function openCloseFunction() {
+    
+    var x = document.getElementById("sidebar");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
