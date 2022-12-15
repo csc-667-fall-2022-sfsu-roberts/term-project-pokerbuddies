50d91259b60898b6c368a459510f61c6b168a555
deadlepricon <rm92691@gmail.com>
2022-12-14 20:51:34 -0800

    fix

--- 667-pokerbuddies/routes/login.js
+++ 667-pokerbuddies/routes/login.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var router = express.Router();
+<<<<<<< HEAD
 var path = require("path");
 var users = require("../db/users");
+=======
+var path = require('path');
+var users = require('../db/users');
+var games = require('../db/games');
+>>>>>>> 5230632df55ec3f9a3768520535ae147da102b70
 const bcrypt = require("bcrypt");
 const { or } = require("sequelize");
 const io = require("socket.io");
@@ -13,6 +19,7 @@ router.get("/", (_request, response) => {
   // response.sendFile(path.join(reqPath, '/FrontEnd/HTML/Login.html'));
 });
 
+<<<<<<< HEAD
 
 //route to Registration page
 router.get("/registration", function (req, res) {
@@ -25,16 +32,50 @@ router.post("/doesUserExist", async function (req, res, next) {
   if (login_username == "" || login_password == "") {
     res.redirect("/login");
   } else {
+=======
+
+ //route to Registration page
+ router.post('/registration', function(req, res) {
+      response.render("public/registration");
+  });
+  
+  router.post('/doesUserExist',async function(req, res, next) {
+    debugger;
+    var login_username = req.body.login_username; 
+    var login_password = req.body.login_password;
+    if(login_username == '' || login_password == ''){
+    res.redirect('/login');
+    }else{
+>>>>>>> 5230632df55ec3f9a3768520535ae147da102b70
     const doesUserExist = await users.doesUserExist(login_username);
     const UsrArr = Object.values(doesUserExist);
 
     console.log(UsrArr);
     //checks if user exists. if not, redirect to error page
+<<<<<<< HEAD
     if (String(UsrArr[0]) == "true") {
       const getUser = await users.getUser(login_username);
       const userInfo = Object.values(getUser);
       bcrypt.compare(login_password, userInfo[2], function (err, result) {
         if (result == true) {
+=======
+    if(String(UsrArr[0])=='true'){
+     const getUser = await users.getUser(login_username)
+     const userInfo = Object.values(getUser)
+      bcrypt.compare(login_password,userInfo[2], function(err,result){
+        if(result == true){
+
+          res.redirect('/joinSession');
+        }
+        else{
+          res.redirect('/login');
+        }
+      }
+      );
+    }else{
+      res.redirect('/userDoesntExist');
+    }}
+>>>>>>> 5230632df55ec3f9a3768520535ae147da102b70
 
           // req.app.io.emit("player-added-login", login_username);
           res.redirect(`/joinSession/${login_username}`);
