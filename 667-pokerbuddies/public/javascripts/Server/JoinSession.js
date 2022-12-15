90d832530b68c150cd06e6a73c0d77ceb8d2dd24
jonathanluu0 <jonathanluu0@gmail.com>
2022-12-14 21:01:19 -0800

    made separate game lobbies

--- 667-pokerbuddies/public/javascripts/Server/JoinSession.js
+++ 667-pokerbuddies/public/javascripts/Server/JoinSession.js
@@ -1,4 +1,7 @@
 const socket = io();
+import { writeFile } from 'fs';
+import { compileFile } from 'pug';
+
 
 
 //holds list of displayed players
@@ -14,11 +17,11 @@ for (let i = 1; i < 6; i++) {
 }
 
 
-//Displays each individual session
+//Displays each individual session and game pages for each session
 const cards = document.querySelectorAll('.card');
 cards.forEach((card, index) => {
     card.children[0].innerText = `Session ${index + 1}`;
-    
+    createGameLobbies(index + 1);
 });
 
 //gives each button an individual id and updates list of players when clicked
@@ -26,28 +29,14 @@ const buttons = document.querySelectorAll('.join');
 buttons.forEach((button, index) => {
     let roomId = index + 1;
     button.children[0].id = roomId;
-
     console.log(roomId);
-    //button.dataset.roomId = roomId
-    
-    // console.log(button.dataset.roomId);
-    // console.log(document.querySelector("#jart").dataset.roomId);
-
-    
     button.addEventListener('click', (event) => {
-        // debugger;
-        fetch(`/games${roomId}`, {//`/games/join/${roomId}`
-            method: "get", 
+        fetch(`/games${roomId}`, {
+            method: "get",
             headers: { "Content-Type": "application/json" },
             // body: JSON.stringify({ id: socket.id})
         })
-        // .then(() => {
-        //     console.log("Fetch request successful? Emptying text box.");
-            
-        // }).catch(error => console.log(error));
-
         // socket.emit('join', roomId);
-       
         updatePlayerList(event);
     });
 }
@@ -81,18 +70,36 @@ const updatePlayerList = (e) => {
     addPlayer();
 }
 
-
-function openCloseFunction() {
-    var x = document.getElementById("sidebar");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+function createGameLobbies(lobbyNum) {
+    if (lobbyNum < 1) {
+        return -1;
     }
-}
+    for (let i = 0; i < lobbyNum; i++) {
+        const newFileName = `games${lobbyNum}.pug`;
+        compileFile('games.pug', (err, originalHTML) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            writeFile(newFileName, originalHTML, (err) => {
+                if (err) {
+                    // Handle the error if there is one
+                    console.error(err);
+                    return;
+                }
+            });
+        });
+    }
 
+    function openCloseFunction() {
+        var x = document.getElementById("sidebar");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
 
-//Brendan's code for testing.
 
 
 
