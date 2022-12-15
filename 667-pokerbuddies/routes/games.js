e88f9855499d583124c2d5c008561fc0518715ed
deadlepricon <rm92691@gmail.com>
2022-12-14 16:11:33 -0800

    game

--- 667-pokerbuddies/routes/games.js
+++ 667-pokerbuddies/routes/games.js
@@ -54,24 +54,24 @@ router.get('/', function(req, res, next) {
   
   res.render("protected/game",{
 
-    // pot: '0000',
+    pot: '0000',
 
-    // player_1_name: player_1_Info.name,
-    // player_1_total: player_1_Info.total_chips,
-    // player_1_bet: player_1_Info.bet,
+    player_1_name: player_1_Info.name,
+    player_1_total: player_1_Info.total_chips,
+    player_1_bet: player_1_Info.bet,
 
-    // player_2_name: player_2_Info.name,
-    // player_2_total: player_2_Info.total_chips,
-    // player_2_bet: player_2_Info.bet,
+    player_2_name: player_2_Info.name,
+    player_2_total: player_2_Info.total_chips,
+    player_2_bet: player_2_Info.bet,
 
-    // player_3_name: player_3_Info.name,
-    // player_3_total: player_3_Info.total_chips,
-    // player_3_bet: player_3_Info.bet,
+    player_3_name: player_3_Info.name,
+    player_3_total: player_3_Info.total_chips,
+    player_3_bet: player_3_Info.bet,
 
-    // player_4_name: player_4_Info.name,
-    // player_4_total: player_4_Info.total_chips,
-    // player_4_bet: player_4_Info.bet,
-    // gameNumber: gameCount,
+    player_4_name: player_4_Info.name,
+    player_4_total: player_4_Info.total_chips,
+    player_4_bet: player_4_Info.bet,
+    gameNumber: gameCount,
 
 
   });
